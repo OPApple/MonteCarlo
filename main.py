b56c5f3749043c5bcc5f214e4d5ad5efa789f1f1
Olli Kankuri <olli.kankuri@gmail.com>
2022-04-20 11:21:33 +0300

    Got the program started

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import random
+
+loop = True
+
+
+while loop == True:
+    #Käyttäjä syöttää luvun
+    N = input("Anna jokin kokonaisluku:\n")
+    N = int(N)
+
+    randoms = []
+
+    #Lisätään satunnaisia kokonaislukuja listaan
+    for i in range(2, N):
+        randoms.append(random.randint(2, int(N/2)))
+        
+    #Muutetaan lista dictionaryksi, jotta saadaan tuplat pois
+    randoms = list(dict.fromkeys(randoms))
+
+    for i in range(0, len(randoms)):
+        
+
+
+
 
