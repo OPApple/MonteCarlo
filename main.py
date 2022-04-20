804f789503f33cb05f9a89066f4e274c362956e0
Olli Kankuri <olli.kankuri@gmail.com>
2022-04-20 12:39:57 +0300

    Added error handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import exception
 import random
 
 loop = True
@@ -6,39 +7,48 @@ loop = True
 while loop == True:
     check = True
 
-    #Käyttäjä syöttää luvun
-    N = input("Anna jokin positiivinen kokonaisluku:\n")
-    N = int(N)
+    try:
+        #Käyttäjä syöttää luvun
+        N = input("\nAnna jokin positiivinen kokonaisluku:\n")
+        N = int(N)
 
-    #Testataan perustapaukset (kun N/2 < 2, mikä aiheuttaa ongelmia satunnaislukujen valinnassa)
-    if N == 2 or N == 3 or N == 5:
-        check = False
-        
-    if N == 4:
-        check = False
-        alkuluku = False
-
-    randoms = []
-    alkuluku = True
-
-    
-    if check == True:
-        #Lisätään satunnaisia kokonaislukuja listaan
-        for i in range(2, N):
-            randoms.append(random.randint(2, int(N/2)))
+        try:
+            if N <= 0:
+                raise Exception("Annettu luku ei ole positiivinen kokonaisluku")
+        except Exception:
+            print("Ei Positiivinen")
+            continue
+        #Testataan perustapaukset (kun N/2 < 2, mikä aiheuttaa ongelmia satunnaislukujen valinnassa)
+        if N == 2 or N == 3 or N == 5:
+            check = False
             
-        #Muutetaan lista dictionaryksi, jotta saadaan tuplat pois
-        randoms = list(dict.fromkeys(randoms))
+        if N == 4:
+            check = False
+            alkuluku = False
 
-        for i in range(0, len(randoms)):
-            if N % randoms[i] == 0:
-                alkuluku = False
-    
-    if alkuluku == False:
-        print("Ei alkuluku")
-    else:
-        print("Alkuluku")
+        randoms = []
+        alkuluku = True
 
+        
+        if check == True:
+            #Lisätään satunnaisia kokonaislukuja listaan
+            for i in range(2, N):
+                randoms.append(random.randint(2, int(N/2)))
+                
+            #Muutetaan lista dictionaryksi, jotta saadaan tuplat pois
+            randoms = list(dict.fromkeys(randoms))
+
+            for i in range(0, len(randoms)):
+                if N % randoms[i] == 0:
+                    alkuluku = False
+        
+        if alkuluku == False:
+            print("Ei alkuluku")
+        else:
+            print("Alkuluku")
+    except ValueError:
+        print("En.")
+        continue
 
 
 
