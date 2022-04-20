f28397a2f4dd4d1c5e75809ee1e49d7753007e62
Olli Kankuri <olli.kankuri@gmail.com>
2022-04-20 12:22:30 +0300

    made the program function

--- main.py
+++ main.py
@@ -4,21 +4,40 @@ loop = True
 
 
 while loop == True:
+    check = True
+
     #Käyttäjä syöttää luvun
-    N = input("Anna jokin kokonaisluku:\n")
+    N = input("Anna jokin positiivinen kokonaisluku:\n")
     N = int(N)
 
+    #Testataan perustapaukset (kun N/2 < 2, mikä aiheuttaa ongelmia satunnaislukujen valinnassa)
+    if N == 2 or N == 3 or N == 5:
+        check = False
+        
+    if N == 4:
+        check = False
+        alkuluku = False
+
     randoms = []
+    alkuluku = True
 
-    #Lisätään satunnaisia kokonaislukuja listaan
-    for i in range(2, N):
-        randoms.append(random.randint(2, int(N/2)))
-        
-    #Muutetaan lista dictionaryksi, jotta saadaan tuplat pois
-    randoms = list(dict.fromkeys(randoms))
+    
+    if check == True:
+        #Lisätään satunnaisia kokonaislukuja listaan
+        for i in range(2, N):
+            randoms.append(random.randint(2, int(N/2)))
+            
+        #Muutetaan lista dictionaryksi, jotta saadaan tuplat pois
+        randoms = list(dict.fromkeys(randoms))
 
-    for i in range(0, len(randoms)):
-        
+        for i in range(0, len(randoms)):
+            if N % randoms[i] == 0:
+                alkuluku = False
+    
+    if alkuluku == False:
+        print("Ei alkuluku")
+    else:
+        print("Alkuluku")
 
 
 
